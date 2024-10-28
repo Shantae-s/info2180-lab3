1a8fca3785ecee206ebccb250073326c9d294a4f
Shantae Saddler <shantaesaddler02@gmail.com>
2024-10-27 23:06:12 -0500

    Added hover function- Excerise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,9 @@
-window.onload=function (){
-    let div_var= document.getElementById("board").children
-    //console.log(div_var)
 let track=[]
-
-    for (var index=0; index < div_var.length; index++){
-        div_var[index].classList.add("square")
 
 function clickAction(ps){
     if (track.length==0 || track[0]=="O"){
         ps.textContent= "X"
-        ps.classList.add("x")
+        ps.classList.add("X")
         track[0]="X"
     }
     else if (track[0]== "X"){
@@ -19,12 +13,21 @@ function clickAction(ps){
     }
 }
 
+function hoveraction(ps){
+    ps.classList.add("hover")
+}
+
+function hoveroutaction(ps){
+    ps.classList.remove("hover")
+}
+
 onload = function(){
     var_div=document.getElementById("board").children
-    
+
     for(let i=0 ;i< var_div.length; i++){
         var_div[i].setAttribute("class","square")
         var_div[i].setAttribute("onclick","clickAction(this)")
+        var_div[i].setAttribute("onmouseover","hoveraction(this)")
+        var_div[i].setAttribute("onmouseout","hoveroutaction(this)")
     }
-    console.log(var_div)
 }
